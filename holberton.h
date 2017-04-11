78d53103ea5c3d37f7b7f192be5ad8c6df640474
Gloria Bwandungi <30@holbertonschool.com>
2017-04-10 20:51:10 -0400

    comments added

--- holberton.h
+++ holberton.h
@@ -1,14 +1,22 @@
-#ifndef LISTS
-#define LISTS
+#ifndef PRINTF
+#define PRINTF
 
-/**/
-
+/* print_char - prints out a single function. */
 int print_char(char c);
+
+/* print_number - prints out a string from an int. */
 void print_number(int n);
+
+/* _printf - prints a formatted string that replaces directives.*/
 int _printf(const char *format, ...);
-char *convert(unsigned int num, int base);
+
+/* str_len - returns the length of a string. */
 int str_len(char *s);
+
+/* print_string - prints out a string using print_char*/
 void print_string(char *str);
+
+/* count_digits - returns the number of digits in an int.*/
 int count_digits(int n);
 
-#endif /* LISTS */
+#endif /* PRINTF */
