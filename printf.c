505e31d93e0100d924d0b2a5a4ebf4706e111bc0
Gloria Bwandungi <30@holbertonschool.com>
2017-04-11 23:17:30 -0400

    empty arguments and % included

--- printf.c
+++ printf.c
@@ -12,12 +12,12 @@
 int _printf(const char *format, ...)
 {
 	const char *p;
-	int i, length;
+	int i, length, fmt_len;
 	char *s;
 	va_list ap;
 
 	va_start(ap, format);
-	length = 0;
+	length = 0; fmt_len = 0;
 	for (p = format; *p != '\0'; p++)
 	{
 		if (*p != '%')
@@ -31,20 +31,24 @@ int _printf(const char *format, ...)
 			case 'c': /* a single character */
 				i = va_arg(ap, int);
 				print_char(i);
-				length++; break;
+				fmt_len++; break;
 			case 's': /* a string */
 				s = va_arg(ap, char *);
+				if (!s)
+				{
+					fmt_len = 0; break;
+				}
 				print_string(s);
-				length += str_len(s); break;
+				fmt_len += str_len(s); break;
 			case '%': /* print the % sign */
 				print_char('%');
 				length++; break;
 			case 'd': case 'i': /* for decimals and integers*/
 				i = va_arg(ap, int);
 				print_number(i);
-				length += count_digits(i); break;
+				fmt_len += count_digits(i); break;
 		}
 	}
 	va_end(ap);
-	return (length);
+	return (length + fmt_len);
 }
--- tests/main2.c
+++ tests/main2.c
@@ -12,19 +12,34 @@ int main(void)
 {
 	char * s = "holberton";
 	char * try = "Let's try to printf a simple sentence.\n";
+	char * nostring = "\0";
 	char c;
-	int len;
-        int len1;
-	int len2;
+	int len, len1, len2, len3, len4, len6;
 
 	c = 'G';
 
         len = _printf("Let's try to printf a simple sentence.\n");
         len1 = printf("Let's try to printf a simple sentence.\n");
 	len2 = str_len(try);
+	len3 = _printf(nostring);
+	len4 = _printf("%%\n");
+	/*len5 = printf(nostring);*/
+	len6 = printf("%%\n");
 
-	_printf("s = %s, c = %c\n", s, c);
-	printf("s = %s, c = %c\n", s, c);
+	_printf("Ours: s = %s, c = %c\n", s, c);
+	printf("Theirs: s = %s, c = %c\n", s, c);
+
+	_printf("Empty String\n");
+	_printf("%s, %i\n", nostring, len3);
+
+	/*printf("Empty String\n");*/
+	/*printf("%%, %s, %i\n", nostring, len5);*/
+
+	_printf("Ours: Percentage\n");
+	_printf("%%, %i\n", len4);
+
+	printf("Theirs: Percentage\n");
+	printf("%%, %i\n", len6);
 
 	printf("try is %d\n", len2);
 
