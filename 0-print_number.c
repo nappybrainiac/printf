e29a812dcac7536e2d1fd2fc0004702a9d6d664a
Gloria Bwandungi <30@holbertonschool.com>
2017-04-12 00:50:01 -0400

    edited last line

--- 0-print_number.c
+++ 0-print_number.c
@@ -20,11 +20,11 @@
 		n *= -1 ; /* to print +ve numbers*/
 	}
 	ncopy = n;
-	for (len = 1; ncopy < -9; len *= 10)
+	for (len = 1; ncopy < -9; len *= 10) /* What multiple of 10?*/
 	{
 		ncopy /= 10;
 	}
-	for (ncopy = n; len != 0; len /= 10)
+	for (ncopy = n; len != 0; len /= 10) /* print each number */
 	{
 		num = (ncopy / len) * -1;
 		print_char(num + '0');
